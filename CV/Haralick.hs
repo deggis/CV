af8028bbb77ac3a1d18c59cd6cc0d115bc3cad40
Heikki Salo <heikki.ao.salo@iki.fi>
2012-03-22 15:16:28 +0200

    Implemented GLCM calculation to Repa DIM2 array.
    Initial implementation of gray-level co-occurrence matrix calculation.
    Performance not tested, most likely awful.

--- CV/Haralick.hs
+++ CV/Haralick.hs
@@ -1,26 +1,40 @@
+{-# LANGUAGE TypeOperators #-}
 module CV.Haralick where
 
 import CV.Image
 
-import qualified Data.Array.CArray as CA
+import qualified Data.Map as M
+import Data.Array.Repa as R
+import Data.Array.Repa.Repr.Unboxed as R
+import Data.List as L
 
-type CoOccurenceMatrix = CA.CArray (Int,Int) Double
+type CoOccurrenceMatrix = R.Array U DIM2 Int
 
-ncolors = 256
+rToC :: Int -> D8 -> Int
+rToC nColors r = round $ realToFrac r * (fromIntegral nColors - 1)
 
-rToC :: Double -> Int
-rToC r = round $ r * (nColors-1)
-
--- | Use stretchHistogram if needed before using this.
-coOccurrenceMatrix :: (Int,Int) -> Image GrayScale D8 -> CoOccurrenceMatrix
-coOccurrenceMatrix (dx,dy) i =
+-- | Probably painfully slow implementation of gray level co-occurrence matrix
+-- (GLCM) calculation to Repa DIM2 array.
+--
+coOccurrenceMatrix :: (Int,Int) -> Int -> Image GrayScale D8 -> CoOccurrenceMatrix
+coOccurrenceMatrix (dx,dy) nColors i =
     let (w,h)  = getSize i
         usableWidth  = if dx > 0 then (w-dx) else w
         usableHeight = if dy > 0 then (h-dy) else h
         startX       = if dx < 0 then (-dx) else 0
         startY       = if dy < 0 then (-dy) else 0
-        getColor = rToC . getPixel
-        relations = [ (getColor (x,y), getColor(x+dx,y+dy))
+        getColor     = rToC nColors . (flip getPixel) i
+        -- Force indices (row,col) to hit bottom triangle of the symmetric DIM2 matrix.
+        swap' (r,c) = if r<c then (c,r) else (r,c)
+        fuu (x,y)    = swap' $ (getColor (x,y), getColor(x+dx,y+dy))
+        neighbours   = [ fuu (x,y)
                             | x <- [startX..(usableWidth-1)]
                             , y <- [startY..(usableHeight-1)] ]
-
+        sums :: M.Map (Int,Int) Int
+        -- XXX: Neighbours sorted as list and packed to temporary Map.
+        sums = M.fromList . Prelude.map (\x -> (head x, length x)) . group . sort $ neighbours
+        shape = Z :. nColors :. nColors
+    in R.computeUnboxedP $ R.fromFunction shape $ \(Z:.r:.c) ->
+                            case M.lookup (swap' (r,c)) sums of
+                                Just sum' -> sum'
+                                _         -> 0
